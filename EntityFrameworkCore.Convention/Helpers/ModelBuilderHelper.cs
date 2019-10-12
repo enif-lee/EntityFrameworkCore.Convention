8f42591b7c6e0a14e53d674e23805b94d62280d2
Enif Lee <enif.lee@gmail.com>
2019-10-12 19:48:49 +0900

    remove unnecessary not implemented method. add facade method for set naming convention conveniently

--- EntityFrameworkCore.Convention/Helpers/ModelBuilderHelper.cs
+++ EntityFrameworkCore.Convention/Helpers/ModelBuilderHelper.cs
@@ -15,14 +15,12 @@ namespace EntityFrameworkCore.Convention.Helpers
 			[EntityState.Deleted] = State.Deleted
 		};
 
-		public static ModelBuilder UseSnakeCaseNamingConvention(this ModelBuilder builder)
-		{
-			// Todo implement not yet;
-			builder.Model.GetEntityTypes();
-
-			return builder;
-		}
-
+		/// <summary>
+		/// 	Configure naming convention for entity frameworks.
+		/// </summary>
+		/// <param name="builder">Model builder of entity entity framework</param>
+		/// <param name="configure">Convention configure setter</param>
+		/// <returns>Origin model builder</returns>
 		public static ModelBuilder UseNamingConvention(this ModelBuilder builder, Action<ConventionBuilder> configure)
 		{
 			var conventionBuilder = new ConventionBuilder();
@@ -30,6 +28,17 @@ namespace EntityFrameworkCore.Convention.Helpers
 
 			conventionBuilder.Apply(builder);
 			return builder;
+		}
+
+		/// <summary>
+		/// 	Apply specific naming convention for all settable name file of entities.
+		/// </summary>
+		/// <param name="builder">Model Builder</param>
+		/// <param name="convention">Convention</param>
+		/// <returns>origin model builder</returns>
+		public static ModelBuilder UseNamingConvention(this ModelBuilder builder, INamingConvention convention)
+		{
+			return builder.UseNamingConvention(cb => cb.UseNamingConvention(convention));
 		}
 
 		public static ChangeTracker UpdateCreatedAtFields(this ChangeTracker tracker)

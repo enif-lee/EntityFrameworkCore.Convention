92e645161c3ef865e3739a2377479f4178ec28bc
Enif Lee <enif.lee@gmail.com>
2020-03-25 01:52:28 +0900

    Add entity logical state extension

---
+++ EntityFrameworkCore.Convention.Test/Fixtures/StateExtension/StateTestEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EntityFrameworkCore.Convention.StateExtension;
+
+namespace EntityFrameworkCore.Convention.Test.Fixtures.StateExtension
+{
+	public class StateTestEntity : IState
+	{
+		[Key]
+		public long Id { get; set; }
+
+		public string Message { get; set; }
+		
+		public State State { get; set; }
+	}
+}
